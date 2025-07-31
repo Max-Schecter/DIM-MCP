496d5e75349ef1d98845635af243a6d6328f120a
Max-Schecter <max.schecter@icloud.com>
2025-07-30 21:42:03 -0700

    mcp-websocket: mark equipped perks

--- src/app/mcp/mcp-websocket.ts
+++ src/app/mcp/mcp-websocket.ts
@@ -72,7 +72,7 @@ function buildWeaponPerkColumns(item: DimItem): string[][] {
           name += ' (Enhanced)';
         }
         if (socket.plugged?.plugDef.hash === p.plugDef.hash) {
-          name += '*';
+          name += ' (equipped)';
         }
         return name;
       }),
